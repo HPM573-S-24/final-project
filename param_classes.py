19e8ad93275b3c33ea2249776bf6550d6e09496e
LeoCai0405 <leonardo.cai@yale.edu>
2024-05-03 18:53:15 -0400

    5

--- param_classes.py
+++ param_classes.py
@@ -17,20 +17,26 @@ class Parameters:
         # initial health state
         self.initialHealthState = data.HealthStates.WELL
 
-        # annual treatment cost
-        if self.test == Tests.PAP_TEST:
-            self.annualTestCost = data.PAP_TEST_COST/3
-        elif self.test == Tests.HPV_TEST:
-            self.annualTestCost = data.HPV_TEST_COST/5
-        elif self.test == Tests.CO_TEST:
-            self.annualTestCost = data.CO_TEST_COST/5
-
         # transition probability matrix of the selected test
         self.probMatrix = data.TRANS_MATRIX
 
         # annual state costs and utilities
         self.annualStateCosts = data.ANNUAL_STATE_COST
         self.annualStateUtilities = data.ANNUAL_STATE_UTILITY
+
+        # annual test cost
+        if self.test == Tests.PAP_TEST:
+            self.annualStateCosts[data.HealthStates.WELL.value] += data.PAP_TEST_COST / 3
+            self.annualStateCosts[data.HealthStates.CIN_1.value] += data.COLPOSCOPY_COST / 3
+            self.annualStateCosts[data.HealthStates.CIN_2plus.value] += data.COLPOSCOPY_COST / 3
+        elif self.test == Tests.HPV_TEST:
+            self.annualStateCosts[data.HealthStates.WELL.value] += data.HPV_TEST_COST / 5
+            self.annualStateCosts[data.HealthStates.CIN_1.value] += data.COLPOSCOPY_COST / 5
+            self.annualStateCosts[data.HealthStates.CIN_2plus.value] += data.COLPOSCOPY_COST / 5
+        elif self.test == Tests.CO_TEST:
+            self.annualStateCosts[data.HealthStates.WELL.value] += data.CO_TEST_COST / 5
+            self.annualStateCosts[data.HealthStates.CIN_1.value] += data.COLPOSCOPY_COST / 5
+            self.annualStateCosts[data.HealthStates.CIN_2plus.value] += data.COLPOSCOPY_COST / 5
 
         # discount rate
         self.discountRate = data.DISCOUNT
